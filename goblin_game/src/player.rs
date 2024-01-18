7a19ced8c35ee928d635e2ca05e1d9bc8adfa1c0
JZims <zimmerman.j@hotmail.com>
2024-01-18 12:01:41 -0500

    Switching over to GDScript

--- goblin_game/src/player.rs
+++ goblin_game/src/player.rs
@@ -1,5 +1,5 @@
 
-use godot::engine::{Area2D, IArea2D, PhysicsBody2D, CollisionShape2D};
+use godot::engine::{Area2D, IArea2D, PhysicsBody2D, CollisionShape2D, Sprite2D};
 use godot::prelude::*;
 
 #[derive(GodotClass)]
@@ -51,43 +51,23 @@ impl IArea2D for Player {
             screen_size: Vector2::new(0.0, 0.0),
             base,
         }
+
     }
 
     fn ready(&mut self) {
         let viewport = self.base().get_viewport_rect();
         self.screen_size = viewport.size;
         self.base_mut();
+        let pos = Vector2::ZERO;
+        
     }
 
     fn process(&mut self, delta: f64) {
-        let sprite = self
-            .base()
-            .get_node_as::<CollisionShape2D>("CollisionShape2D");
-
-        let mut velocity = Vector2::new(0.0, 0.0);
-
         
-        let input = Input::singleton();
-        if input.is_action_pressed("right".into()) {
-            velocity += Vector2::RIGHT;
-        }
-        if input.is_action_pressed("left".into()) {
-            velocity += Vector2::LEFT;
-        }
-        if input.is_action_pressed("down".into()) {
-            velocity += Vector2::DOWN;
-        }
-        if input.is_action_pressed("up".into()) {
-            velocity += Vector2::UP;
-        }
+          
+     }
+      
+       
+    
 
-
-        let change = velocity * real::from_f64(delta);
-        let position = self.base().get_global_position() + change;
-        let position = Vector2::new(
-            position.x.clamp(0.0, self.screen_size.x),
-            position.y.clamp(0.0, self.screen_size.y),
-        );
-        self.base_mut().set_global_position(position);
-    }
 }
